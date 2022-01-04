8ae95825f6ef451f3a54519f6f6b23796e979ceb
sequws <workspaceshare@gmail.com>
2022-01-04 22:59:15 +0100

    Add Recipe model - represents the dish and its full calorie count

---
+++ MyMacros/Shell/Models/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        // Always per 100g
+        public double Fats { get; set; }
+        public double Carbs { get; set; }
+        public double Proteins { get; set; }
+        public double Cellulose { get; set; }
+        // Additional information
+        public string Infos { get; set; }
+
+        // Weight of full recipe
+        public double Weight { get; set; }
+        public double PieceWeight { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public double Kcal => 4 * (Carbs + Proteins) + 9 * Fats + 2 * Cellulose;
+    }
+}
