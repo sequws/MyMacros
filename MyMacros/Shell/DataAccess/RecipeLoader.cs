827654e667f0616d2d46dc043e757cfe5eeb7ddd
sequws <workspaceshare@gmail.com>
2022-02-03 23:20:24 +0100

    Add RecipeLoader

---
+++ MyMacros/Shell/DataAccess/RecipeLoader.cs
@@ -0,0 +1,24 @@
+﻿using Core.Abstractions;
+using Core.Interfaces;
+using Shell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell.DataAccess
+{
+    public class RecipeLoader : Loader, ILoader<Recipe>
+    {
+        public RecipeLoader() : base("recipes")
+        {
+
+        }
+
+        public IEnumerable<Recipe> LoadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
